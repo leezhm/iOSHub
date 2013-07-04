113af37c1e777058eac050de4b11ea8ef750f59e
leezhm <leezhm@126.com>
2013-07-04 10:48:24 +0800

    basic project Quiz

---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Zhengmin.li on 7/4/13.
+//  Copyright (c) 2013 Zhengmin.li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/QuizTests/QuizTests.h
@@ -0,0 +1,13 @@
+//
+//  QuizTests.h
+//  QuizTests
+//
+//  Created by Zhengmin.li on 7/4/13.
+//  Copyright (c) 2013 Zhengmin.li. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QuizTests : SenTestCase
+
+@end
