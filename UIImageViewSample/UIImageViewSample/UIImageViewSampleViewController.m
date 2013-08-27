b824205ec810f0cbff3b56487e26830fbb9e25e1
leezhm <leezhm@126.com>
2013-08-28 01:09:04 +0800

    show a image with UIImageView

--- UIImageViewSample/UIImageViewSample/UIImageViewSampleViewController.m
+++ UIImageViewSample/UIImageViewSample/UIImageViewSampleViewController.m
@@ -18,6 +18,17 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // create a UIImageView
+    UIImage * img = [UIImage imageNamed:@"Resource/magic"];
+    NSLog(@"image width:%f and height:%f", [img size].width, [img size].height);
+    
+    UIImageView * imgView = [[UIImageView alloc] initWithImage:img];
+    [imgView setFrame:CGRectMake(0, 0, [img size].width, [img size].height)];
+    [imgView setBackgroundColor:[UIColor redColor]];
+    
+    // add sub-view
+    [self.view addSubview:imgView];
 }
 
 - (void)didReceiveMemoryWarning
