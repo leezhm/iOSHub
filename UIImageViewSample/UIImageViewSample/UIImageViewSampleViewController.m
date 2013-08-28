a167ce9db06226c44247eebfd7b0eed8553fe682
zhengmin.li <leezhm@126.com>
2013-08-28 14:14:20 +0800

    show a squence png as a animation with UIImageView.

--- UIImageViewSample/UIImageViewSample/UIImageViewSampleViewController.m
+++ UIImageViewSample/UIImageViewSample/UIImageViewSampleViewController.m
@@ -19,19 +19,41 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    // create a UIImageView
-    UIImage * img = [UIImage imageNamed:@"Resource/magic"];
+//    // create a UIImageView
+//    UIImage * img = [UIImage imageNamed:@"Resource/magic"];
+//
+//    NSLog(@"image width:%f and height:%f", [img size].width, [img size].height);
+//    NSLog(@"Main View Center x:%f and y:%f", [self.view center].x, [self.view center].y);
+//    NSLog(@"Main View width:%f and height:%f", [self.view bounds].size.width, [self.view bounds].size.height);
+//
+//    UIImageView * imgView = [[UIImageView alloc] initWithImage:img];
+//    [imgView setFrame:CGRectMake(0, 0, [self.view bounds].size.width, [self.view bounds].size.height)];
+//    [imgView setBackgroundColor:[UIColor redColor]];
+//
+//    // add sub-view
+//    [self.view addSubview:imgView];
     
-    NSLog(@"image width:%f and height:%f", [img size].width, [img size].height);
-    NSLog(@"Main View Center x:%f and y:%f", [self.view center].x, [self.view center].y);
-    NSLog(@"Main View width:%f and height:%f", [self.view bounds].size.width, [self.view bounds].size.height);
     
-    UIImageView * imgView = [[UIImageView alloc] initWithImage:img];
-    [imgView setFrame:CGRectMake(0, 0, [self.view bounds].size.width, [self.view bounds].size.height)];
-    [imgView setBackgroundColor:[UIColor redColor]];
+    // UIImage Animation
+    UIImageView * AnimationView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [self.view bounds].size.width, [self.view bounds].size.height)];
     
-    // add sub-view
-    [self.view addSubview:imgView];
+    NSMutableArray * muArray = [[NSMutableArray alloc] initWithCapacity:80];
+    
+    for (int i = 1; i <= 80; ++ i) {
+        
+        NSString * file = [NSString stringWithFormat:@"Resource/CompleteAnimation/%i", i];
+        
+        //NSLog(@"%@", file);
+        
+        [muArray addObject:[UIImage imageNamed:file]];
+    }
+    
+    [AnimationView setAnimationImages:muArray];
+    [AnimationView setAnimationDuration:3.0];
+    
+    [self.view addSubview:AnimationView];
+    
+    [AnimationView startAnimating];
 }
 
 - (void)didReceiveMemoryWarning
