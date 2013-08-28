ddbfe7c30dc79311338762cb2012bd2f9db552ed
leezhm <leezhm@126.com>
2013-08-28 23:56:35 +0800

    taking a photo, it can edit the picture and then show the image.

--- UIImagePickerControllerSample/UIImagePickerControllerSample/UIImagePickerControllerViewController.m
+++ UIImagePickerControllerSample/UIImagePickerControllerSample/UIImagePickerControllerViewController.m
@@ -8,7 +8,7 @@
 
 #import "UIImagePickerControllerViewController.h"
 
-@interface UIImagePickerControllerViewController ()
+@interface UIImagePickerControllerViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
@@ -31,9 +31,30 @@
 {
     UIImagePickerController * ipc = [[UIImagePickerController alloc] init];
     
+    // delegate
+    ipc.delegate = self;
+    
+    // Can Editing
+    [ipc setAllowsEditing:YES];
+    
     // set the source
     [ipc setSourceType:UIImagePickerControllerSourceTypeCamera];
     [self presentViewController:ipc animated:YES completion:nil];
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    // dismiss the taking-a-photo view
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    // get the picture
+    UIImage * img = info[UIImagePickerControllerEditedImage];
+    
+    NSLog(@"image type : %@", info[UIImagePickerControllerMediaType]);
+    NSLog(@"image URL : %@", info[UIImagePickerControllerMediaURL]);
+    NSLog(@"image ref URL : %@", info[UIImagePickerControllerReferenceURL]);
+    
+    [self.imageView setImage:img];
 }
 
 @end
