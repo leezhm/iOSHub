f63baa427b32b83f1832e68e45ab734e4cf8bd39
zhengmin.li <leezhm@126.com>
2013-09-03 18:09:08 +0800

    new project --- storyboardsample

---
+++ StoryboardSample/StoryboardSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryboardSample
+//
+//  Created by Hsee.com on 9/3/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StoryboardSampleAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StoryboardSampleAppDelegate class]));
+    }
+}
