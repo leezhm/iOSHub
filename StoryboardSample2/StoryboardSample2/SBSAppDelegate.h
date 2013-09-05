47070787c711a4d4f54aa61190b1b960d4ed697c
zhengmin.li <leezhm@126.com>
2013-09-05 13:58:29 +0800

    new project -- storyboardsample2

---
+++ StoryboardSample2/StoryboardSample2/SBSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SBSAppDelegate.h
+//  StoryboardSample2
+//
+//  Created by Hsee.com on 9/5/13.
+//  Copyright (c) 2013 Jerry.li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
