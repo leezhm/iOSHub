b19f0ac0065d03cc52f2bbda663b5b2f011ce6cd
zhengmin.li <leezhm@126.com>
2013-09-10 19:10:51 +0800

    add a '弹入弹出' button

--- CoreAnimationSample1/CoreAnimationSample1/CAViewController.m
+++ CoreAnimationSample1/CoreAnimationSample1/CAViewController.m
@@ -19,13 +19,33 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    // set the background
+    [self.view setBackgroundColor:[UIColor purpleColor]];
     
+    // add a button
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+
+    [btn setTitle:@"弹出弹入" forState:UIControlStateNormal];   
+    btn.frame = CGRectMake(20, 20, 60, 30); // set size of button
+    
+    // set the touchupinside event and event handle function    
+    [btn addTarget:self action:@selector(btnMoveInAndOutForUIView) forControlEvents:UIControlEventTouchUpInside];
+    
+    // add/show the button to current view
+    [self.view addSubview:btn];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)btnMoveInAndOutForUIView
+{
+    // Move out
+    
 }
 
 @end
