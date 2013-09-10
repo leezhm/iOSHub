6fbe73c71b11b7b24de6f5283efc72abfa463577
zhengmin.li <leezhm@126.com>
2013-09-10 16:27:44 +0800

    new project -- CoreAnimationSample1

---
+++ CoreAnimationSample1/CoreAnimationSample1/CAViewController.m
@@ -0,0 +1,31 @@
+//
+//  CAViewController.m
+//  CoreAnimationSample1
+//
+//  Created by Hsee.com on 9/10/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import "CAViewController.h"
+
+@interface CAViewController ()
+
+@end
+
+@implementation CAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
