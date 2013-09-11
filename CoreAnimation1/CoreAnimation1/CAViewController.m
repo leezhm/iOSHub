2197a4983ea9f679fd207292d33d9ae778aeffbf
leezhm <leezhm@126.com>
2013-09-11 22:26:33 +0800

    detect device version and show a image.

--- CoreAnimation1/CoreAnimation1/CAViewController.m
+++ CoreAnimation1/CoreAnimation1/CAViewController.m
@@ -18,6 +18,46 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // set the background
+    [self.view setBackgroundColor:[UIColor purpleColor]];
+    
+    // detect the version of current device
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    
+    NSString * imgPath = @"";
+    
+//    // method 1
+//    if(DBL_EPSILON > fabs(bounds.size.height - (double)568.0))
+//    {
+//        NSLog(@"iPhone 5 ...");
+//    }
+//    else if(DBL_EPSILON > fabs(bounds.size.height - (double)480.0))
+//    {
+//        NSLog(@"iPhone4/4s ...");
+//    }
+    
+    // method 2
+    if(CGSizeEqualToSize(CGSizeMake(320.0, 568.0), bounds.size))
+    {
+        NSLog(@"iPhone 5 ...");
+        imgPath = @"iOS5";
+    }
+    else if(CGSizeEqualToSize(CGSizeMake(320.0, 480.0), bounds.size))
+    {
+        NSLog(@"iPhone 4/4s ...");
+        imgPath = @"iOS4";
+    }
+    
+    // create a UIImage
+    UIImage * img = [UIImage imageNamed:imgPath];
+    
+    // create a UIImageView
+    UIImageView * imgView = [[UIImageView alloc] initWithImage:img];
+    [imgView setFrame:CGRectMake(0, 0, bounds.size.width, bounds.size.height)];
+    
+    // add this image to current view
+    [self.view addSubview:imgView];
 }
 
 - (void)didReceiveMemoryWarning
