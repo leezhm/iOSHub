7368d7d349f221b6525333bea94ee2645350898e
leezhm <leezhm@126.com>
2013-09-11 23:04:22 +0800

    add new button

--- CoreAnimation1/CoreAnimation1/CAViewController.m
+++ CoreAnimation1/CoreAnimation1/CAViewController.m
@@ -23,9 +23,9 @@
     [self.view setBackgroundColor:[UIColor purpleColor]];
     
     // detect the version of current device
-    CGRect bounds = [[UIScreen mainScreen] bounds];
-    
-    NSString * imgPath = @"";
+//    CGRect bounds = [[UIScreen mainScreen] bounds];
+//    
+//    NSString * imgPath = @"";
     
 //    // method 1
 //    if(DBL_EPSILON > fabs(bounds.size.height - (double)568.0))
@@ -37,33 +37,64 @@
 //        NSLog(@"iPhone4/4s ...");
 //    }
     
-    // method 2
-    if(CGSizeEqualToSize(CGSizeMake(320.0, 568.0), bounds.size))
-    {
-        NSLog(@"iPhone 5 ...");
-        imgPath = @"iOS5";
-    }
-    else if(CGSizeEqualToSize(CGSizeMake(320.0, 480.0), bounds.size))
-    {
-        NSLog(@"iPhone 4/4s ...");
-        imgPath = @"iOS4";
-    }
+//    // method 2
+//    if(CGSizeEqualToSize(CGSizeMake(320.0, 568.0), bounds.size))
+//    {
+//        NSLog(@"iPhone 5 ...");
+//        imgPath = @"iOS5";
+//    }
+//    else if(CGSizeEqualToSize(CGSizeMake(320.0, 480.0), bounds.size))
+//    {
+//        NSLog(@"iPhone 4/4s ...");
+//        imgPath = @"iOS4";
+//    }
+//    
+//    // create a UIImage
+//    UIImage * img = [UIImage imageNamed:imgPath];
+//    
+//    // create a UIImageView
+//    UIImageView * imgView = [[UIImageView alloc] initWithImage:img];
+//    [imgView setFrame:CGRectMake(0, 0, bounds.size.width, bounds.size.height)];
+//    
+//    // add this image to current view
+//    [self.view addSubview:imgView];
     
-    // create a UIImage
-    UIImage * img = [UIImage imageNamed:imgPath];
+    // add a button to current view
+    UIButton * btnDropImageView = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [btnDropImageView setFrame:CGRectMake(40, 40, 80, 40)];
+    [btnDropImageView setTitle:@"弹入弹出" forState:UIControlStateNormal];
+    [btnDropImageView addTarget:self action:@selector(btnDropImageViewEventHandler) forControlEvents:UIControlEventTouchUpInside];
     
-    // create a UIImageView
-    UIImageView * imgView = [[UIImageView alloc] initWithImage:img];
-    [imgView setFrame:CGRectMake(0, 0, bounds.size.width, bounds.size.height)];
-    
-    // add this image to current view
-    [self.view addSubview:imgView];
+    // add
+    [self.view addSubview:btnDropImageView];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)btnDropImageViewEventHandler
+{
+    //
+    NSString * imgPath = @"";
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    
+    if(CGSizeEqualToSize(bounds.size, CGSizeMake(320.0, 568.0)))
+    {
+        imgPath = @"iOS5";
+    }
+    else if(CGSizeEqualToSize(bounds.size, CGSizeMake(320.0, 480.0)))
+    {
+        imgPath = @"iOS4";
+    }
+    
+    UIImageView * imgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imgPath]];
+    [imgView setFrame:CGRectMake(0, 0, bounds.size.width, bounds.size.height)];
+
+    [self.view addSubview:imgView];
+
 }
 
 @end
