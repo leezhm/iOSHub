fb3dc6b8aca64b164e2506830ddebf2ed1369f30
zhengmin.li <leezhm@126.com>
2013-09-12 13:40:52 +0800

    set the background as a image

--- CoreAnimation2/CoreAnimation2/CAViewController.m
+++ CoreAnimation2/CoreAnimation2/CAViewController.m
@@ -10,6 +10,8 @@
 
 @interface CAViewController ()
 
+@property (nonatomic, weak) NSString * device;
+
 @end
 
 @implementation CAViewController
@@ -18,6 +20,26 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Get the device version
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    if(CGSizeEqualToSize(bounds.size, CGSizeMake(320.0, 568.0f)))
+    {
+        NSLog(@"iOS5 ...");
+        self.device = @"iOS5";
+    }
+    else if(CGSizeEqualToSize(bounds.size, CGSizeMake(320.0, 480.0)))
+    {
+        NSLog(@"iOS4 ...");
+        self.device = @"iOS4";
+    }
+    
+    // load a image as background
+    NSString * file = [NSString stringWithFormat:@"Images/%@", self.device];
+    NSLog(@"file path -> %@", file);
+    UIColor * backgroundColor = [[UIColor alloc] initWithPatternImage:[UIImage imageNamed:file]];
+    self.view.backgroundColor = backgroundColor;
+    
 }
 
 - (void)didReceiveMemoryWarning
