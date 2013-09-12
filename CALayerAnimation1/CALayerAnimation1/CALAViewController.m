aba22a4f0521ef60c1cee00781ee1465a429cce5
zhengmin.li <leezhm@126.com>
2013-09-12 20:28:27 +0800

    new project -- CALayerAnimation1

---
+++ CALayerAnimation1/CALayerAnimation1/CALAViewController.m
@@ -0,0 +1,29 @@
+//
+//  CALAViewController.m
+//  CALayerAnimation1
+//
+//  Created by Hsee.com on 9/12/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import "CALAViewController.h"
+
+@interface CALAViewController ()
+
+@end
+
+@implementation CALAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
