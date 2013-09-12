dd6f498bc016e2345da2b3310770ece07a0b8f65
zhengmin.li <leezhm@126.com>
2013-09-12 16:33:49 +0800

    set view layer as a UIImage

--- CALayerSample1/CALayerSample1/CALViewController.m
+++ CALayerSample1/CALayerSample1/CALViewController.m
@@ -8,6 +8,8 @@
 
 #import "CALViewController.h"
 
+#import <QuartzCore/QuartzCore.h>
+
 @interface CALViewController ()
 
 @end
@@ -19,7 +21,21 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    NSString * file = @"";
     
+    if(CGSizeEqualToSize(bounds.size, CGSizeMake(320.0f, 568.0f)))
+    {
+        file = @"Images/iOS5";
+    }
+    else if(CGSizeEqualToSize(bounds.size, CGSizeMake(320.0f, 480.0f)))
+    {
+        file = @"Images/iOS4";
+    }
+    
+    //
+    UIImage * img = [UIImage imageNamed:file];
+    self.view.layer.contents = (__bridge id)(img.CGImage);
 }
 
 - (void)didReceiveMemoryWarning
