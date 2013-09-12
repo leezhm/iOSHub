5ccb59d0013a2b836a01f390e0f9cc45b8bbfc58
zhengmin.li <leezhm@126.com>
2013-09-12 15:20:33 +0800

    new project -- CALayerSample1

---
+++ CALayerSample1/CALayerSample1/CALAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CALAppDelegate.h
+//  CALayerSample1
+//
+//  Created by Hsee.com on 9/12/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CALViewController;
+
+@interface CALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CALViewController *viewController;
+
+@end
---
+++ CALayerSample1/CALayerSample1/CALViewController.m
@@ -0,0 +1,29 @@
+//
+//  CALViewController.m
+//  CALayerSample1
+//
+//  Created by Hsee.com on 9/12/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import "CALViewController.h"
+
+@interface CALViewController ()
+
+@end
+
+@implementation CALViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
