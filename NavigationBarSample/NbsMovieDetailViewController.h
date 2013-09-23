3116f69e772eb2476bdc9dd24a6f33232213debb
zhengmin.li <leezhm@126.com>
2013-09-23 22:02:27 +0800

    fixed all bugs

---
+++ NavigationBarSample/NbsMovieDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  NbsMovieDetailViewController.h
+//  NavigationBarSample
+//
+//  Created by Hsee.com on 9/23/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NbsMovieDetailViewController : UIViewController
+
+@end
