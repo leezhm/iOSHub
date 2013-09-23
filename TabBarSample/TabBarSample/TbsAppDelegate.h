0b667f1f19e8f1a55ad5f533ae92bd317c57d3c7
zhengmin.li <leezhm@126.com>
2013-09-23 15:16:47 +0800

    new project -- TabBarSample

---
+++ TabBarSample/TabBarSample/TbsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TbsAppDelegate.h
+//  TabBarSample
+//
+//  Created by Hsee.com on 9/23/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TbsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
