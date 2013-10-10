07ca7f1444ea18a1e9c91d4e08df3f7f0f6ef4e3
leezhm <leezhm@126.com>
2013-10-10 23:48:56 +0800

    new project -- SpriteKitGameSample1

---
+++ SpriteKitGameSample1/SpriteKitGameSample1/SKGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SKGAppDelegate.h
+//  SpriteKitGameSample1
+//
+//  Created by Zhengmin.li on 10/10/13.
+//  Copyright (c) 2013 Jerry.li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKitGameSample1/SpriteKitGameSample1/SKGMyScene.h
@@ -0,0 +1,13 @@
+//
+//  SKGMyScene.h
+//  SpriteKitGameSample1
+//
+
+//  Copyright (c) 2013 Jerry.li. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SKGMyScene : SKScene
+
+@end
