880b2b546f833d4f314e4791b6671978dcd3feda
zhengmin.li <leezhm@126.com>
2013-10-14 10:00:38 +0800

    new project -- RayStoryboardSample

---
+++ RayStoryboardSample1/RayStoryboardSample1/RayAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RayAppDelegate.h
+//  RayStoryboardSample1
+//
+//  Created by Hsee.com on 10/14/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RayAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RayStoryboardSample1/RayStoryboardSample1/RayViewController.m
@@ -0,0 +1,29 @@
+//
+//  RayViewController.m
+//  RayStoryboardSample1
+//
+//  Created by Hsee.com on 10/14/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import "RayViewController.h"
+
+@interface RayViewController ()
+
+@end
+
+@implementation RayViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
