b6797591658e75e7b82ab4cfa7f77b98ef91ec70
zhengmin.li <leezhm@126.com>
2013-10-21 15:16:23 +0800

    new project -- UIPageControllerSample

---
+++ UIPageControllerSample/UIPageControllerSample/UPCViewController.m
@@ -0,0 +1,29 @@
+//
+//  UPCViewController.m
+//  UIPageControllerSample
+//
+//  Created by Hsee.com on 10/21/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import "UPCViewController.h"
+
+@interface UPCViewController ()
+
+@end
+
+@implementation UPCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ UIPageControllerSample/UIPageControllerSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIPageControllerSample
+//
+//  Created by Hsee.com on 10/21/13.
+//  Copyright (c) 2013 code4fun.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UPCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UPCAppDelegate class]));
+    }
+}
