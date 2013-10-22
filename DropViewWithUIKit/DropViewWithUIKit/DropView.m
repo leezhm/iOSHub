dfe9e2e35eb2d526f419662fff0e40d5c77518dd
zhengmin.li <leezhm@126.com>
2013-10-22 23:31:13 +0800

    drop animation

--- DropViewWithUIKit/DropViewWithUIKit/DropView.m
+++ DropViewWithUIKit/DropViewWithUIKit/DropView.m
@@ -8,6 +8,14 @@
 
 #import "DropView.h"
 
+#import <UIKit/UIKit.h>
+
+@interface DropView ()
+
+@property (nonatomic, strong) UIDynamicAnimator * animator;
+
+@end
+
 @implementation DropView
 
 - (id)initWithFrame:(CGRect)frame
@@ -27,6 +35,16 @@
         self.backgroundColor = [[UIColor alloc] initWithPatternImage:img];
         
         NSLog(@"Self Bounds %@", NSStringFromCGRect(self.bounds));
+        
+        // init the UIKit
+        self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.superview];
+        
+        // Assume the view is offscreen. Use a Snap behaviour to position it in the center of the screen.
+        UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+        
+        // Adjust our keyWindow's tint adjustment mode to make everything behind the alert view dimmed
+        keyWindow.tintAdjustmentMode = UIViewTintAdjustmentModeDimmed;
+        [keyWindow tintColorDidChange];
     }
     return self;
 }
@@ -47,20 +65,59 @@
     
     [keyWindow addSubview:self];
     
+    // Adjust our keyWindow's tint adjustment mode to make everything behind the alert view dimmed
+    keyWindow.tintAdjustmentMode = UIViewTintAdjustmentModeDimmed;
+    [keyWindow tintColorDidChange];
+    
     NSLog(@"Key Window %@, Description : %@", NSStringFromCGRect(self.frame), keyWindow.description);
     
     // Animate in the background blind
-    [UIView animateWithDuration:3.4f animations:^{
+    
+    self.alpha = 0.0f;
+    [UIView animateWithDuration:0.1f animations:^{
         self.alpha = 1.0f;
         NSLog(@"alpha = %f", self.alpha);
     }];
     
-    
+    // Use UIKit Dynamics to make the alertView appear.
+    UISnapBehavior *snapBehaviour = [[UISnapBehavior alloc] initWithItem:self snapToPoint:keyWindow.center];
+    snapBehaviour.damping = 0.45f;
+    [self.animator addBehavior:snapBehaviour];
 }
 
 - (void)dissView
 {
+    // Assume that the view is currently in the center of the screen. Add some gravity to make it disappear.
+    // It *should* disappear before the animation fading away the background completes.
+    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
     
+    [self.animator removeAllBehaviors];
+    
+    UIGravityBehavior *gravityBehaviour = [[UIGravityBehavior alloc] initWithItems:@[self]];
+    gravityBehaviour.gravityDirection = CGVectorMake(0.0f, 12.0f);
+    [self.animator addBehavior:gravityBehaviour];
+    
+    UIDynamicItemBehavior *itemBehaviour = [[UIDynamicItemBehavior alloc] initWithItems:@[self]];
+    [itemBehaviour addAngularVelocity:-M_PI_2 forItem:self];
+    [self.animator addBehavior:itemBehaviour];
+    
+    // Animate out our background blind
+    [UIView animateWithDuration:0.8f animations:^{
+        self.alpha = 0.0f;
+        keyWindow.tintAdjustmentMode = UIViewTintAdjustmentModeAutomatic;
+        [keyWindow tintColorDidChange];
+    } completion:^(BOOL finished) {
+        [self removeFromSuperview];
+        // Very important!
+        //self.retainedSelf = nil;
+    }];
+
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self dissView];
+    NSLog(@"event : %@", event.description);
 }
 
 @end
